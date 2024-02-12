7338e2e7f033d804486e64b1b170055bda1b11f2
sollien <m-sollien@hotmail.com>
2024-02-12 08:48:46 +0100

    Photography

--- src/assets/images/photography/index.ts
+++ src/assets/images/photography/index.ts
@@ -1,7 +1,41 @@
 import deadTree from "./dead_tree.jpg"
 import fjellhaugenSmall from "./fjellhaugen_small.jpg"
+import folgefonnaBaatSmall from "./folgefonna_baat_small.jpg"
+import folgefonnaFjellSmall from "./folgefonna_fjell_small.jpg"
+import folgefonnaSteinerSmall from "./folgefonna_steiner_small.jpg"
+import hotAirBalloonSmall from "./hot_air_balloon_small.jpg"
+import latourellFallsDistanceSmall from "./latourell_falls_distance_small.jpg"
+import laatefossen1Small from "./laatefossen_1_small.jpg"
+import laatefossen2Small from "./laatefossen_2_small.jpg"
+import latourellFallsSmall from "./latourell_falls_small.jpg"
+import mbSmall from "./mb_small.jpg"
+import oceanNeedles from "./ocean_needles_small.jpg"
+import osloEkeberg from "./oslo_ekeberg.jpg"
+import powerLinesSmall from "./power_lines_small.jpg"
+import saltholmen1 from "./saltholmen_1.jpg"
+import saltholmen2Small from "./saltholmen_2_small.jpg"
+import spaceNeedle from "./space_needle.jpg"
+import starSkyGCSmall from "./star_sky_gc_small.jpg"
+import surveyorSmall from "./surveyor_small.jpg"
 
 export {
 	deadTree,
 	fjellhaugenSmall,
+	folgefonnaBaatSmall,
+	folgefonnaFjellSmall,
+	folgefonnaSteinerSmall,
+	hotAirBalloonSmall,
+	latourellFallsDistanceSmall,
+	laatefossen1Small,
+	laatefossen2Small,
+	latourellFallsSmall,
+	mbSmall,
+	oceanNeedles,
+	osloEkeberg,
+	powerLinesSmall,
+	saltholmen1,
+	saltholmen2Small,
+	spaceNeedle,
+	starSkyGCSmall,
+	surveyorSmall
 }

aa55d976cf5bba5ba09a0167228ce8ec30bf170b
Martin <51482460+Sollien@users.noreply.github.com>
2024-11-07 17:53:53 +0100

    Svelte and MDX integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
+import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [tailwind(), svelte(), mdx()],
 	output: "server",
 	adapter: netlify(),
 	prefetch: true

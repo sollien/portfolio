0f0f8c8b4ea81da2729d3ec17100cc5636eec5f4
Martin <51482460+Sollien@users.noreply.github.com>
2024-11-07 23:54:36 +0100

    Svelte chart test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
-import svelte from "@astrojs/svelte";
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
+import netlify from "@astrojs/netlify"
+import svelte from "@astrojs/svelte"
+import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), svelte(), mdx()],
 	output: "server",
 	adapter: netlify(),
-	prefetch: true
+	prefetch: true,
 });

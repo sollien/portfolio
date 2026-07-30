18f004a50b18aa188823f550926c9fb062968aa0
Martin Sollien <m-sollien@hotmail.com>
2026-07-30 15:54:22 +0200

    Ny test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-import netlify from "@astrojs/netlify"
 import svelte from "@astrojs/svelte"
 import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), svelte(), mdx()],
-	output: "server",
-	adapter: netlify(),
+	output: "static",
 	prefetch: true,
 });
